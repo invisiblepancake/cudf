ecf1ce65b4ad8df6a02afcbf625b0cf3e5ca11d6
Vyas Ramasubramani <vyasr@nvidia.com>
2024-12-02 20:23:35 +0000

    Pass as CPM args

--- cpp/cmake/thirdparty/get_nanoarrow.cmake
+++ cpp/cmake/thirdparty/get_nanoarrow.cmake
@@ -14,11 +14,6 @@
 
 # This function finds nanoarrow and sets any additional necessary environment variables.
 function(find_and_configure_nanoarrow)
-  include(${rapids-cmake-dir}/cpm/package_override.cmake)
-
-  # set(cudf_patch_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/patches")
-  # rapids_cpm_package_override("${cudf_patch_dir}/nanoarrow_override.json")
-
   if(NOT BUILD_SHARED_LIBS)
     set(_exclude_from_all EXCLUDE_FROM_ALL FALSE)
   else()
@@ -36,12 +31,12 @@ function(find_and_configure_nanoarrow)
   rapids_cpm_find(
     nanoarrow 0.7.0.dev
     GLOBAL_TARGETS nanoarrow
+    CPM_ARGS
     GIT_REPOSITORY https://github.com/apache/arrow-nanoarrow.git
     GIT_TAG e54b7df525fa1d310a96687bd99902823402b26c
     GIT_SHALLOW FALSE
-    CPM_ARGS
-    OPTIONS "BUILD_SHARED_LIBS OFF" "NANOARROW_NAMESPACE cudf" "NANOARROW_DEBUG ${_nanarrow_debug}"
-            ${_exclude_from_all}
+    OPTIONS "BUILD_SHARED_LIBS OFF" "NANOARROW_NAMESPACE cudf"
+            "NANOARROW_DEBUG ${_nanoarrow_debug}" ${_exclude_from_all}
   )
   set_target_properties(nanoarrow PROPERTIES POSITION_INDEPENDENT_CODE ON)
   rapids_export_find_package_root(BUILD nanoarrow "${nanoarrow_BINARY_DIR}" EXPORT_SET cudf-exports)
